5a0a4af342d9a4e7ec3b196058ace559664813d0
Haunted-Bone <dmcguffey92@gmail.com>
2024-08-28 11:50:06 -0700

    Initial Commit

---
+++ lab1.py
@@ -0,0 +1,39 @@
+import math
+
+radius = 5
+Area =(math.pi*(radius**2))
+
+radius = 3
+Volume = ((4/3)*math.pi*(radius**3))
+
+a = 3
+b = 4
+Hypotenuse = math.sqrt(a**2+b**2)
+
+print('The area of the circle is',Area)
+print('The volume of the sphere is', Volume)
+print('The hypotenuse of the triangle is', Hypotenuse)
+
+
+
+myname = 'Dylan'+'McGuffey'
+
+
+print ('The length of my name is', len(myname), 'characters.')
+print(myname.upper())
+print(myname.lower())
+
+myAge = 32
+myHeight = 6
+myWeight = 165
+
+BMI = ((myWeight/((myHeight*12)+2)**2)*703)
+
+print('The Variable "My Age" is', type(myAge))
+print('The Variable "My Height" is', type(myHeight))
+print('The Variable "My Weight" is', type(myWeight))
+print('My BMI is', BMI)
+
+
+
+
